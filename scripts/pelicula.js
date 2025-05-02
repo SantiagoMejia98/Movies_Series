4d97e9e3311913b482d4cf572cc21fdc013afbd3
SantyMejia20 <saamejiaga@unal.edu.co>
2025-05-01 20:32:24 -0500

    Update pelicula.js

--- scripts/pelicula.js
+++ scripts/pelicula.js
@@ -231,19 +231,18 @@ function crearPelicula(elemento, datos) {
     traduccionesStatus[datos.Status]
   }</p>
     <p><i>${datos.Tagline ? datos.Tagline : ""}</i></p> 
-    ${
-      datos.Videos.length > 0
-        ? `<ul>
-      <li class="movie-item">
-        <img src="https://image.tmdb.org/t/p/w92/pTnn5JwWr4p3pG8H6VrpiQo7Vs0.jpg">
-        <p>Ver tráiler</p>
+    <ul>
+    <li class="movie-item">
+    <a href="https://www.youtube.com/results?search_query=${
+      datos.Nombre +
+      " " +
+      (datos.Lanzamiento !== 9999 ? datos.Lanzamiento : "") +
+      " trailer"
+    }" target="_blank"><img src="https://image.tmdb.org/t/p/w92/pTnn5JwWr4p3pG8H6VrpiQo7Vs0.jpg">
+    </a>
       </li>
     </ul>
-    
-    ${datos.Descripcion ? `<h3>Sinopsis</h3>` : ""}
-    <p>${datos.Descripcion}</p>`
-        : ""
-    }
+    ${datos.Descripcion ? `<h3>Sinopsis</h3> ${datos.Descripcion}` : ""}
     `;
 
   const proveedores = document.createElement("div");
